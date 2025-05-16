661c5980a349841d4fecac6de062b609d5f1f19e
arjunpkulkarni <arjunpkulkarni@gmail.com>
2025-05-16 11:35:04 -0400

    Refactor: Organize components into individual folders and fix paths

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './App.css';
 
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import ImageGallery from './components/ImageGallery';
-import Packages from './components/Packages';
-import FAQ from './components/FAQ';
-import Booking from './components/Booking';
-import CallToAction from './components/CallToAction';
-import Footer from './components/Footer';
+import Navbar from './components/Navbar/Navbar';
+import Hero from './components/Hero/Hero';
+import ImageGallery from './components/ImageGallery/ImageGallery';
+import Packages from './components/Packages/Packages';
+import FAQ from './components/FAQ/FAQ';
+import Booking from './components/Booking/Booking';
+import CallToAction from './components/CallToAction/CallToAction';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
